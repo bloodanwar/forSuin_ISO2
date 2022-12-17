2cd9b9cdbf5d06fabab44a970bd2405e6346bcac
Sergio García <sergio.garciia.munoz@gmail.com>
2022-12-17 18:57:19 +0100

    Update test

--- src/test/java/negocio/controllers/PantallaPropuestaCursoTest.java
+++ src/test/java/negocio/controllers/PantallaPropuestaCursoTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.awt.Button;
 import java.awt.Color;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -29,7 +30,7 @@ public class PantallaPropuestaCursoTest {
 	
 	@Test
 	public void PantallaPropuestaCursoTest1() {
-		int action = 2;
+		int action = 0;
 		ProfesorUCLM director = new ProfesorUCLM("23568907X");
 		
 		String id = "id";
@@ -57,6 +58,51 @@ public class PantallaPropuestaCursoTest {
 		
 		pantalla.getSendBto().doClick();
 
+	}
+	
+	@Test
+	public void PantallaPropuestaCursoTest2() {
+		int action = 1;
+		ProfesorUCLM director = new ProfesorUCLM("23568907X");
+		
+		String id = "id";
+		String nombre = "nombre";
+		int ECTS = 20;
+		Date fechaInicio = new Date();
+		Date fechaFin = new Date();
+		int tasaMatricula = 10;
+		int edicion = 0;
+		EstadoCurso estado = EstadoCurso.EN_IMPARTIZICION;
+		TipoCurso tipo = TipoCurso.CORTA_DURACION;
+		Centro centro = new Centro("UCLM TAL");
+		ProfesorUCLM secretario = new ProfesorUCLM("23568907X");
+		ProfesorUCLM director1 = new ProfesorUCLM("23568907X");
+		String requisitos = "requisito";
+		
+		CursoPropio curso = new CursoPropio(id, nombre,ECTS, fechaInicio, fechaFin, tasaMatricula, edicion, estado, tipo, centro, secretario, director1, requisitos);
+		curso.materias = new ArrayList<Materia>();
+		curso.materias.add(new Materia("materia", 20, new Date(), new Date(), new ProfesorUCLM("23568907X")));
+		
+		pantalla = new PantallaPropuestaCurso(director, curso, action);
+		
+		try {
+			curso.cursoPropioDao.crearNuevoCurso(curso);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		JTextField titulo = (JTextField) pantalla.getComponentByName("tituloBox");
+		assertEquals(nombre, titulo.getText());
+		
+		pantalla.getSendBto().doClick();
+		
+		try {
+			curso.cursoPropioDao.eliminarCursoPropio(curso);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	@Test
