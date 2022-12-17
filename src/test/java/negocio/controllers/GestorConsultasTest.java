6aea5a6b52cbfcde2fa342bf0da32795d90514c4
rubnium <rubengomezvillegas@gmail.com>
2022-12-17 16:47:43 +0100

    2 test creados donde deben de estar

---
+++ src/test/java/negocio/controllers/GestorConsultasTest.java
@@ -0,0 +1,52 @@
+package negocio.controllers;
+
+import org.junit.Test;
+
+public class GestorConsultasTest {
+
+	@Test
+	public void testConsultarIngresos() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testConsultarEstadoCursos() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testListarEdicionesCursos() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testListarCursosPorDirector() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testListarCursosPorEstado() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testListarTodosCursos() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testListarProfesores() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testListarProfesoresUCLM() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testListarCentros() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
