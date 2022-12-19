425ee4981272bc058c8760ca2f05a7087d332bb3
rubnium <rubengomezvillegas@gmail.com>
2022-12-19 22:08:47 +0100

    Revert "Update profesores tests"
    This reverts commit 87e197425f72669d856ab969612ade1d52b6d8af.

--- src/test/java/persistencia/ProfesorDAOTest.java
+++ src/test/java/persistencia/ProfesorDAOTest.java
@@ -15,40 +15,24 @@ public class ProfesorDAOTest {
 	
 	@Test
 	public void testCrearNuevoProfesor() throws SQLException {
-		// PRUEBA DE CREACION PROFESOR
 		Profesor profesorEsperado = new Profesor("dni", "nombre", "apellido", true);
 		List<Profesor> profesoresEsperados = profesorEsperado.profesorDao.listarProfesores();
 		profesoresEsperados.add(profesorEsperado);
 		int position = profesoresEsperados.size()-1;
 
+		
 		profesorEsperado.profesorDao.crearNuevoProfesor(profesorEsperado);
 		List<Profesor> profesoresObtenidos = profesorEsperado.profesorDao.listarProfesores();
 		Profesor profesorObtenido = profesoresObtenidos.get(position);
 		
 		assertEquals(profesoresEsperados.size(), profesoresObtenidos.size());
+		
 		assertEquals(profesorEsperado.getNombre(), profesorObtenido.getNombre());
 		assertEquals(profesorEsperado.getDni(), profesorObtenido.getDni());
 		assertEquals(profesorEsperado.getApellidos(), profesorObtenido.getApellidos());
 		assertEquals(profesorEsperado.isDoctor(), profesorObtenido.isDoctor());
 
-		// PRUEBA DE SELECCIONAR PROFESOR
-		profesorObtenido = profesorObtenido.profesorDao.seleccionarProfesor(profesorEsperado);
-		assertEquals(profesorEsperado.getNombre(), profesorObtenido.getNombre());
-		assertEquals(profesorEsperado.getDni(), profesorObtenido.getDni());
-		assertEquals(profesorEsperado.getApellidos(), profesorObtenido.getApellidos());
-		assertEquals(profesorEsperado.isDoctor(), profesorObtenido.isDoctor());
-		
-		// PRUEBA DE EDITAR PROFESOR
-				
-		
-		
-		// PRUEBA DE ELIMINACION PROFESOR
 		profesorEsperado.profesorDao.eliminarProfesor(profesorObtenido);
-		profesoresEsperados.remove(position);
-		profesoresObtenidos = profesorEsperado.profesorDao.listarProfesores();
-
-		assertEquals(profesoresEsperados.size(), profesoresObtenidos.size());
-
 	}
 
 	@Test
